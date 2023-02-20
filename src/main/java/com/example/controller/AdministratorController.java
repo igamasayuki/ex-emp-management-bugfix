58582bcb54e298678cc1286d9c8bac456f128a43
17matsumoto <17yuya.baseball@gmail.com>
2023-02-21 00:14:12 +0900

    ログイン者名表示

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -114,6 +114,8 @@ public class AdministratorController {
 			redirectAttributes.addFlashAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return "redirect:/";
 		}
+		session.setAttribute("administratorName", administrator.getName());
+
 		return "redirect:/employee/showList";
 	}
 
