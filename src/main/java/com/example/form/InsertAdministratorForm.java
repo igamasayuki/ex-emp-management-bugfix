3d81f90d06543bf5f8be1f4f1bf1442abecae53a
mh214 <orrtrrttr@gmail.com>
2023-06-08 17:41:40 +0900

    管理者登録画面の入力値エラー処理を実装

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,14 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "氏名は必須です")
 	private String name;
 	/** メールアドレス */
+	@Email(message = "メールアドレスを入力してください")
+	@NotBlank(message = "メールアドレスは必須です")
 	private String mailAddress;
 	/** パスワード */
+	@Size(min = 8, max = 16, message = "8字以上16字以内で設定してください。")
 	private String password;
 
 	public String getName() {
