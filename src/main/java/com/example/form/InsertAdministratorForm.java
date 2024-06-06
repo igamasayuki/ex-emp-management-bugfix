86ca11697314f8e103fd9e58cbdf8e976d90f3a1
Ruii1112 <ruiiiii1112@gmail.com>
2024-06-06 15:48:28 +0900

    管理者登録の入力値チェック
    AdministratorControllerのtoInsertメソッドの引数追加
    
    InsertAdministratorFormに入力値チェック追加
    
    AdministratorControllerのinsertメソッドをバリデーションに対応
    
    InsertAdministratorFormに入力値チェックの修正

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +13,15 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前は必須入力です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message = "メールアドレスは必須入力です")
+	@Email(message = "メールアドレスの形式が不正です")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message = "パスワードは必須入力です")
+	@Size(min = 8, message = "パスワードは8文字以上で入力してください")
 	private String password;
 
 	public String getName() {
