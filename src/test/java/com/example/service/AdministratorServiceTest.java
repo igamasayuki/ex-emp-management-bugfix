fe2a25ad2c5b094375b5799e5b2219794c49022c
suzukikoei <koeich6@gmail.com>
2025-06-06 17:23:05 +0900

    7-1 管理者サービスクラスのテストを新規作成

---
+++ src/test/java/com/example/service/AdministratorServiceTest.java
@@ -0,0 +1,88 @@
+package com.example.service;
+
+import com.example.domain.Administrator;
+import com.example.repository.AdministratorRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class AdministratorServiceTest {
+
+    @Mock
+    private AdministratorRepository administratorRepository;
+
+    @InjectMocks
+    private AdministratorService administratorService;
+
+    @Test
+    void insert() {
+        Administrator mockAdministrator = new Administrator();
+        Integer mockID = 999;
+        String mockName = "MockAdministrator";
+        String mockMailAddress = "mock@mock.com";
+        String mockPassword = "mockPassword";
+        mockAdministrator.setId(mockID);
+        mockAdministrator.setName(mockName);
+        mockAdministrator.setMailAddress(mockMailAddress);
+        mockAdministrator.setPassword(mockPassword);
+
+        administratorService.insert(mockAdministrator);
+        verify(administratorRepository,
+                times(1)).insert(mockAdministrator);
+    }
+
+    /**
+     * ログイン成功.
+     */
+    @Test
+    void login_success() {
+        Administrator mockAdministrator = new Administrator();
+        Integer mockID = 999;
+        String mockName = "MockAdministrator";
+        String mockMailAddress = "mock@mock.com";
+        String mockPassword = "mockPassword";
+        mockAdministrator.setId(mockID);
+        mockAdministrator.setName(mockName);
+        mockAdministrator.setMailAddress(mockMailAddress);
+        mockAdministrator.setPassword(mockPassword);
+
+        when(administratorRepository.findByMailAddressAndPassward(mockMailAddress,mockPassword )).thenReturn(mockAdministrator);
+
+        Administrator administrator = administratorService.login(mockMailAddress,mockPassword);
+        assertNotNull(administrator);
+        assertEquals(administrator.getName(), mockName, "名前：期待された結果と異なります");
+    }
+
+    /**
+     * パスワード不一致によるログイン失敗
+     */
+    @Test
+    void login_failure_for_password() {
+        Administrator mockAdministrator = new Administrator();
+        Integer mockID = 999;
+        String mockName = "MockAdministrator";
+        String mockMailAddress = "mock@mock.com";
+        String mockActualPassword = "correct";
+        String mockWrongPassword = "wrong";
+        mockAdministrator.setId(mockID);
+        mockAdministrator.setName(mockName);
+        mockAdministrator.setMailAddress(mockMailAddress);
+        mockAdministrator.setPassword(mockActualPassword);
+
+        when(administratorRepository.findByMailAddressAndPassward(mockMailAddress,mockActualPassword)).thenReturn(mockAdministrator);
+
+        when(administratorRepository.findByMailAddressAndPassward(mockMailAddress,mockWrongPassword)).thenReturn(null);
+
+        Administrator actualAdministrator = administratorService.login(mockMailAddress, mockActualPassword);
+
+        Administrator wrongAdministrator = administratorService.login(mockMailAddress, mockWrongPassword);
+
+        assertNotNull(actualAdministrator);
+        assertNull(wrongAdministrator);
+    }
+}
