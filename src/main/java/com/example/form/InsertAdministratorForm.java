52329ab4ea2d2da7dc1d1f44876b5caa7da193c1
suinmi24 <yjadeone@gmail.com>
2025-06-06 17:56:59 +0900

    入力値エラー処理の実装

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -7,11 +12,19 @@ package com.example.form;
  * 
  */
 public class InsertAdministratorForm {
+
 	/** 名前 */
+	@NotBlank(message="値を入力してください")
+	@Size(min=1, max=20, message="名前は1文字以上20文字以下で入力してください")
 	private String name;
+
 	/** メールアドレス */
+	@NotBlank(message="値を入力してください")
+	@Size(min=1, max=20, message="メールアドレスは1文字以上20文字以下で入力してください")
 	private String mailAddress;
+
 	/** パスワード */
+	@NotEmpty(message="値を入れてください")
 	private String password;
 
 	public String getName() {
