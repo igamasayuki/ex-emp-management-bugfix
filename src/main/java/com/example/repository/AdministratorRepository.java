0d250ee06d59e4f04fa13ed0552a901125b1c886
YokerHS <hiimyoker@gmail.com>
2025-06-09 14:24:04 +0900

    可読性改善のため、sqlを修正

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -57,10 +57,17 @@ public class AdministratorRepository {
 	 * @param password    パスワード
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
-	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address= '" + mailAddress
-				+ "' and password='" + password + "'";
-		SqlParameterSource param = new MapSqlParameterSource();
+	public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
+		String sql = """
+				select id,name,mail_address,password
+				from administrators
+				where mail_address= :mailAddress
+				and password= :password
+				""";
+		SqlParameterSource param =
+				new MapSqlParameterSource()
+						.addValue("mailAddress",mailAddress)
+						.addValue("password",password);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
 			return null;
@@ -81,12 +88,16 @@ public class AdministratorRepository {
 
 	/**
 	 * メールアドレスから管理者情報を取得します.
-	 * 
+	 *
 	 * @param mailAddress メールアドレス
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
 	public Administrator findByMailAddress(String mailAddress) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress";
+		String sql = """
+				select id,name,mail_address,password 
+				from administrators 
+				where mail_address=:mailAddress
+				""";
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
