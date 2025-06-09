d286216f14bdb3cb32dded0a6392a108b005cf58
YokerHS <hiimyoker@gmail.com>
2025-06-09 13:32:20 +0900

    adminのフィールド条件追加

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,16 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
+	@Pattern(regexp = "^.{0,15}$", message = "名前は15文字以内につけてください")
 	private String name;
 	/** メールアドレス */
+	@Email(message = "不正なメールアドレスです")
+	@NotBlank(message = "メールアドレスを入力してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Pattern(regexp = "^.{5,10}$", message = "パスワードは5文字以上12文字以内で入力してください")
 	private String password;
 
 	public String getName() {
