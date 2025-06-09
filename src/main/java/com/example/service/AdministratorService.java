79591df817fb7bad524bacbd14833ac4fc9bbd16
YokerHS <hiimyoker@gmail.com>
2025-06-09 14:27:30 +0900

    isAdminExists()の追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -31,13 +31,23 @@ public class AdministratorService {
 
 	/**
 	 * ログインをします.
-	 * 
+	 *
 	 * @param mailAddress メールアドレス
 	 * @param password    パスワード
 	 * @return 管理者情報 存在しない場合はnullが返ります
 	 */
 	public Administrator login(String mailAddress, String password) {
-		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
+		Administrator administrator = administratorRepository.findByMailAddressAndPassword(mailAddress, password);
 		return administrator;
 	}
+
+	/**
+	 * 指定されたメールアドレスを持つ管理者が存在するかどうかを判定する。
+	 *
+	 * @param mailAddress 管理者のメールアドレス
+	 * @return 該当する管理者が存在すれば {@code true}、存在しなければ {@code false}
+	 */
+	public boolean isAdminExists(String mailAddress) {
+        return administratorRepository.findByMailAddress(mailAddress) != null;
+    }
 }
