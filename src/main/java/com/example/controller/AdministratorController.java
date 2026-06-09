6e4adf5c40b77f673e23fd469b7d85bf8f5d5e8a
simeiro <shitimi1633@gmail.com>
2026-06-09 15:54:37 +0900

    fix: 入力値エラー処理の実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -69,7 +71,10 @@ public class AdministratorController {
      * @return ログイン画面へリダイレクト
      */
     @PostMapping("/insert")
-    public String insert(InsertAdministratorForm form) {
+    public String insert(@Validated InsertAdministratorForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "administrator/insert";
+        }
         Administrator administrator = new Administrator();
         // フォームからドメインにプロパティ値をコピー
         BeanUtils.copyProperties(form, administrator);
@@ -89,7 +94,7 @@ public class AdministratorController {
     public String toLogin() {
         return "administrator/login";
     }
-	
+
     /**
      * ログインします.
      *
