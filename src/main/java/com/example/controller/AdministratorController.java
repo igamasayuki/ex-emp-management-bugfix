bb145819fef5f2ed62f9e89cbf835195dabbf613
RomaruDaze <romarudazee99@gmail.com>
2026-06-09 10:54:42 +0900

    FIX : ログイン画面の入力がエラーが出た時に、入力した文字はフォームに残す処理を修正しました。＃2

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -8,10 +8,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -86,7 +84,10 @@ public class AdministratorController {
      * @return ログイン画面
      */
     @GetMapping("/")
-    public String toLogin() {
+    public String toLogin(Model model) {
+        if (!model.containsAttribute("form")) {
+            model.addAttribute("form", new LoginForm());
+        }
         return "administrator/login";
     }
 	
@@ -97,10 +98,11 @@ public class AdministratorController {
      * @return ログイン後の従業員一覧画面
      */
     @PostMapping("/login")
-    public String login(LoginForm form, RedirectAttributes redirectAttributes) {
+    public String login(LoginForm form, RedirectAttributes redirectAttributes, Model model) {
         Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
         if (administrator == null) {
             redirectAttributes.addFlashAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
+            redirectAttributes.addFlashAttribute("form", form);
             return "redirect:/";
         }
         return "redirect:/employee/showList";
