7bc6b930d43d912ada416423f42252cf493ba261
simeiro <shitimi1633@gmail.com>
2026-06-09 15:21:45 +0900

    fix: 管理者情報登録後、ログイン画面にリダイレクトするように修正

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -74,7 +74,7 @@ public class AdministratorController {
         // フォームからドメインにプロパティ値をコピー
         BeanUtils.copyProperties(form, administrator);
         administratorService.insert(administrator);
-        return "employee/list";
+        return "redirect:/";
     }
 
     /////////////////////////////////////////////////////
@@ -89,7 +89,7 @@ public class AdministratorController {
     public String toLogin() {
         return "administrator/login";
     }
-	
+
     /**
      * ログインします.
      *
