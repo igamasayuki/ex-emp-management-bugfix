b776f2ffd2b05495bcffd6d54c1a088f342be0d4
tainoasa <tainoasa@gmail.com>
2026-06-09 15:45:20 +0900

    (1-2)初級 入力値エラー処理

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,7 @@
 package com.example.form;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  *
@@ -9,16 +11,19 @@ public class InsertAdministratorForm {
     /**
      * 名前
      */
+    @NotBlank(message = "名前は必須です")
     private String name;
     /**
      * メールアドレス
      */
+    @NotBlank(message = "メールアドレスは必須です")
     private String mailAddress;
     /**
      * パスワード
      */
+    @NotBlank(message = "パスワードは必須です")
     private String password;
-	
+
     public String getName() {
         return name;
     }
