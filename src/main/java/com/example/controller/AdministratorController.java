3e510e0d0644a6ae1df3569402c87981a11d25c0
simeiro <shitimi1633@gmail.com>
2026-06-10 14:39:35 +0900

    fix: 存在しているメールアドレスで登録しようとした際にエラーメッセージが表示されるように修正

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -60,7 +61,7 @@ public class AdministratorController {
      * @return 管理者登録画面
      */
     @GetMapping("/toInsert")
-    public String toInsert(InsertAdministratorForm form) {
+    public String toInsert(InsertAdministratorForm form, Model model) {
         return "administrator/insert";
     }
 
@@ -71,13 +72,20 @@ public class AdministratorController {
      * @return ログイン画面へリダイレクト
      */
     @PostMapping("/insert")
-    public String insert(@Validated InsertAdministratorForm form, BindingResult result) {
+    public String insert(@Validated InsertAdministratorForm form, BindingResult result, Model model) {
         if (result.hasErrors()) {
-            return toInsert(form);
+            return toInsert(form, model);
         }
         Administrator administrator = new Administrator();
         // フォームからドメインにプロパティ値をコピー
         BeanUtils.copyProperties(form, administrator);
+
+        // 存在するメールアドレスか確認
+        String mailAddress = administrator.getMailAddress();
+        if (administratorService.existMailAddress(mailAddress)) {
+            model.addAttribute("existMailAddressMessage", "そのメールアドレスは既に存在しています");
+            return toInsert(form, model);
+        }
         administratorService.insert(administrator);
         return "redirect:/";
     }
